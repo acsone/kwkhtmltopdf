4c6506e9a3053873277622719f5a5e08081da146
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-03-02 17:55:04 +0100

    add some tests

---
+++ tests/kwkhtmltopdf_test.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+"""
+Before running these tests,
+* start kwkhtmltopdf_server
+* set KWKHTMLTOPDF_SERVER_URL environment variable
+"""
+
+import os
+import re
+import subprocess
+import tempfile
+
+import pytest
+from wand.image import Image
+
+
+HERE = os.path.dirname(__file__)
+
+
+class Client:
+    def __init__(self, cmd):
+        self.cmd = cmd
+
+    def _run_stdout(self, args, check_return_code=True):
+        proc = subprocess.Popen(
+            self.cmd + args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+        )
+        out, _ = proc.communicate()
+        if check_return_code:
+            assert proc.returncode == 0, out
+        return out
+
+    def _run_expect_error(self, args):
+        r = subprocess.call(self.cmd + args)
+        assert r != 0
+
+    def _run_expect_file(self, args, expected_data_file):
+        f = tempfile.NamedTemporaryFile(delete=False)
+        try:
+            f.close()
+            r = subprocess.call(
+                self.cmd + args + [f.name], cwd=os.path.join(HERE, "data")
+            )
+            assert r == 0
+            expected = Image(filename=os.path.join(HERE, "data", expected_data_file))
+            actual = Image(filename=f.name)
+            diff = actual.compare(expected, metric="root_mean_square")
+            assert diff[1] < 0.01
+        finally:
+            os.remove(f.name)
+
+
+@pytest.fixture(params=["native", "client"], scope="module")
+def client(request):
+    if request.param == "native":
+        yield Client(["wkhtmltopdf"])
+    elif request.param == "client":
+        yield Client(
+            [
+                os.path.join(HERE, "..", "client", "kwkhtmltopdf_client.py"),
+            ]
+        )
+
+
+def test_noargs(client):
+    out = client._run_stdout([], check_return_code=False)
+    assert "Synopsis:" in out
+    assert "Headers And Footer Options:" not in out
+
+
+def test_help(client):
+    out = client._run_stdout(["-h"])
+    assert "Synopsis:" in out
+    assert "Headers And Footer Options:" not in out
+
+
+def test_extended_help(client):
+    out = client._run_stdout(["--extended-help"])
+    assert "Synopsis:" in out
+    assert "Headers And Footer Options:" in out
+
+
+def test_version(client):
+    out = client._run_stdout(["--version"])
+    assert re.search("wkhtmltopdf [\d\.]+ ", out)
+
+
+def test_1(client):
+    client._run_expect_file(["test1.html"], "test1.pdf")
